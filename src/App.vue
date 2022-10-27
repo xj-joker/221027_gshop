e1fe629bbc3ac466ad9e28e01243ba4b1547930e
joker <3431296802@qq.com>
2022-10-27 16:17:39 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+  </div>
+</template>
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
